7f6e0c5793264a212ce5484b707ab5e98f7f6994
Mario Flach <info@almightycouch.org>
2016-02-01 11:33:59 +0100

    Add :ecto and :rethinkdb dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule RethinkDB.Ecto.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :ecto]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,7 @@ defmodule RethinkDB.Ecto.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ecto, "~> 1.1.3"},
+     {:rethinkdb, ">= 0.0.0"}]
   end
 end
