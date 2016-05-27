c9d5fbbd703f4f6832119b5e851ee5138a689c9d
Mario Flach <info@almightycouch.org>
2016-05-27 22:48:25 +0200

    Add support for integration tests

--- test/rethinkdb_ecto_test.exs
+++ test/rethinkdb_ecto_test.exs
@@ -1,13 +1,7 @@
-defmodule RethinkDB.EctoTest do
+defmodule RethinkDB.Ecto.Test do
   use ExUnit.Case
-  doctest RethinkDB.Ecto
 
-  import Ecto.Query, only: [from: 2]
-  import Ecto.Changeset, only: [cast: 4]
-
-  defmodule Repo do
-    use Ecto.Repo, otp_app: :rethinkdb_ecto
-  end
+  alias Ecto.Integration.TestRepo
 
   defmodule User do
     use Ecto.Schema
@@ -18,102 +12,28 @@ defmodule RethinkDB.EctoTest do
       field :name, :string
       field :age, :integer
       field :in_relationship, :boolean
+      timestamps
     end
   end
 
-  @users [%{name: "Mario", age: 26, in_relationship: true},
-          %{name: "Sophie", age: 29, in_relationship: false},
-          %{name: "Peter", age: 20, in_relationship: false},
-          %{name: "Lara", age: 25, in_relationship: true}]
-
   setup_all do
-    import Supervisor.Spec
-    import RethinkDB.Query
-
-    # Start the Repo as worker of the supervisor tree
-    Supervisor.start_link([supervisor(Repo, [])], strategy: :one_for_one)
-
-    # Clear table
-    table("users")
-    |> delete
-    |> Repo.run
-
-    # Bulk insert users
-    table("users")
-    |> insert(@users)
-    |> Repo.run
-
     :ok
   end
 
-  test "fetches all" do
-    users = Repo.all(User)
-    names = Enum.map(users, &Map.get(&1, :name))
-
-    assert Enum.all?(@users, &(&1.name in names))
-  end
-
-  test "fetches all ordered by age (asc)" do
-    users = Repo.all(from u in User, order_by: u.age)
-    names = Enum.map(users, &Map.get(&1, :name))
-
-    assert names == ["Peter", "Lara", "Mario", "Sophie"]
-  end
-
-  test "fetches all ordered by age (desc)" do
-    users = Repo.all(from u in User, order_by: [desc: u.age])
-    names = Enum.map(users, &Map.get(&1, :name))
-
-    assert names == ["Sophie", "Mario", "Lara", "Peter"]
-  end
-
-  test "filters singles only" do
-    users = Repo.all(from u in User, where: not u.in_relationship)
-    names = Enum.map(users, &Map.get(&1, :name))
-
-    assert \
-      Enum.filter(@users, &(not &1.in_relationship))
-      |> Enum.all?(&(&1.name in names))
-  end
-
-  test "filters people allowed to drink alcohol in US" do
-    users = Repo.all(from u in User, where: u.age > 20)
-    names = Enum.map(users, &Map.get(&1, :name))
-
-    assert \
-      Enum.filter(@users, &(&1.age > 20))
-      |> Enum.all?(&(&1.name in names))
-  end
-
-  test "fetches all and select id and name only" do
-    users = Repo.all(from u in User, select: [u.id, u.name])
-    names = Enum.map(@users, &Map.get(&1, :name))
-
-    assert length(users) == length(@users)
-    for [_id, name] <- users, do: assert name in names
-  end
-
-  test "counts users" do
-    [count] = Repo.all(from u in User, select: count(u.id))
-    assert count == 4
-  end
-
-  test "computes average of all users age " do
-    [avg] = Repo.all(from u in User, select: avg(u.age))
-    assert avg == 25
-  end
-
   test "insert, update and delete user" do
-    user = Repo.insert!(%User{name: "Hugo", age: 20, in_relationship: false})
-    assert user.name == "Hugo"
+    user_params = %{name: "Mario", age: 26, in_relationship: true}
+    {:ok, user} =
+      Ecto.Changeset.cast(%User{}, user_params, Map.keys(user_params))
+      |> TestRepo.insert
+    assert ^user_params = Map.take(user, Map.keys(user_params))
 
-    changeset = cast(user, %{in_relationship: true}, ~w(name age in_relationship), ~w())
-    user = Repo.update!(changeset)
-    assert user.in_relationship == true
+    user_params = Map.put(user_params, :in_relationship, false)
+    {:ok, user} =
+      Ecto.Changeset.cast(user, user_params, Map.keys(user_params))
+      |> TestRepo.update
+    assert ^user_params = Map.take(user, Map.keys(user_params))
 
-    Repo.delete!(user)
-    assert_raise Ecto.NoResultsError, fn ->
-      Repo.get_by!(User, name: "Hugo")
-    end
+    {:ok, user} = TestRepo.delete user
+    assert ^user_params = Map.take(user, Map.keys(user_params))
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,52 @@
 ExUnit.start()
+
+Application.put_env(:ecto, :primary_key_type, :binary_id)
+
+Code.require_file "../deps/ecto/integration_test/support/repo.exs", __DIR__
+Code.require_file "../deps/ecto/integration_test/support/schemas.exs", __DIR__
+Code.require_file "../deps/ecto/integration_test/support/migration.exs", __DIR__
+
+# Pool repo for async, safe tests
+alias Ecto.Integration.TestRepo
+
+Application.put_env(:ecto, TestRepo,
+  adapter: RethinkDB.Ecto,
+  database: "test")
+
+defmodule Ecto.Integration.TestRepo do
+  use Ecto.Integration.Repo, otp_app: :ecto
+end
+
+# Pool repo for non-async tests
+alias Ecto.Integration.PoolRepo
+
+Application.put_env(:ecto, PoolRepo,
+  adapter: RethinkDB.Ecto,
+  database: "test",
+  pool_size: 10,
+  max_restarts: 20,
+  max_seconds: 10)
+
+defmodule Ecto.Integration.PoolRepo do
+  use Ecto.Integration.Repo, otp_app: :ecto
+
+  def create_prefix(prefix) do
+    "create schema #{prefix}"
+  end
+
+  def drop_prefix(prefix) do
+    "drop schema #{prefix}"
+  end
+end
+
+defmodule Ecto.Integration.Case do
+  use ExUnit.CaseTemplate
+end
+
+{:ok, _pid} = TestRepo.start_link
+{:ok, _pid} = PoolRepo.start_link
+
+_   = Ecto.Storage.down(TestRepo)
+:ok = Ecto.Storage.up(TestRepo)
+
+:ok = Ecto.Migrator.up(TestRepo, 0, Ecto.Integration.Migration, log: false)
